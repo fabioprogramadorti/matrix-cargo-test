de9c7ec41933e8a52701e9417a85a8d014a6c28b
fabioprogramadorti <fabioprogramadorti@gmail.com>
2021-01-22 15:40:21 -0300

    Add static paths function

--- pages/repos/[lang].js
+++ pages/repos/[lang].js
@@ -1,4 +1,5 @@
 import Layout from '../../components/layout'
+import { getAllLanguagesAliases } from '../../lib/repos'
 
 export default function Lang() {
 	return (
@@ -6,4 +7,17 @@ export default function Lang() {
 		
 		</Layout>
 	)
+}
+
+export async function getStaticPaths() {
+	// Return a list of possible value for lang
+	const paths = getAllLanguagesAliases()
+	return {
+		paths,
+		fallback: false
+	}
+}
+
+export async function getStaticProps({ params }) {
+  // Fetch necessary data for the repos using params.lang
 }
