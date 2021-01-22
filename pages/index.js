05d357927cfd0754272cb583f2bd3428900028af
fabioprogramadorti <fabioprogramadorti@gmail.com>
2021-01-22 14:58:24 -0300

    Add style to layout

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Layout from '../components/layout'
 
 export default function Home() {
   return (
-    <Head>
-      <title>Github Finder</title>
-      <link rel="icon" href="/GitHub-Mark.png" />
-    </Head>
+    <Layout>
+      <Head>
+        <title>Github Finder</title>
+        <link rel="icon" href="/GitHub-Mark.png" />
+      </Head>
+      <h1>Github Finder</h1>
+    </Layout>
   )
 }
