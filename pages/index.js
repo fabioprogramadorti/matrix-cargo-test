5dc19fba445649cc2f45e568348b59b5f8775cef
fabioprogramadorti <fabioprogramadorti@gmail.com>
2021-01-22 15:27:41 -0300

    Fetch languages data from api

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,34 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getAllLanguages } from '../lib/repos'
 
-export default function Home() {
+export default function Home({ languages }) {
 	return (
 		<Layout home>
 			<Head>
 				<title>{siteTitle}</title>
 			</Head>
 			<section className={utilStyles.headingMd}>
-				All laguages here
+				<ul className={utilStyles.list}>
+					{languages.map(({ name, aliases }) => (
+						<li className={utilStyles.listItem} key={aliases[0]}>
+							{name}
+						</li>
+					))}
+				</ul>
 			</section>
 
 		</Layout>
 	)
+}
+
+export async function getStaticProps() {
+	const languages = await getAllLanguages()
+
+	return {
+		props: {
+			languages
+		}
+	}
 }
